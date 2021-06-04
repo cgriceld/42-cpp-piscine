1c4f0e19d4c920e6fc79031596ec18dcd4d28bd1
Clefable Gricelda <cgriceld@ox-o1.msk.21-school.ru>
2021-06-04 16:41:36 +0300

    add sed

--- module_1/ex07/main.cpp
+++ module_1/ex07/main.cpp
@@ -2,27 +2,31 @@
 #include <string>
 #include <fstream>
 
-static void sed(std::ifstream file, std::ofstream replace, std::string to_find, std::string to_replace)
+static void sed(std::ifstream &file, std::ofstream &replace, std::string &to_find, std::string &to_replace)
 {
-	size_t pos = 0;
+	size_t i = 0;
 	std::string line;
 
 	while (std::getline(file, line))
 	{
-		while ((pos = line.find(to_find)) != std::string::npos)
+		while ((i = line.find(to_find)) != std::string::npos)
 		{
-			line.replace(pos, to_find.length(), to_replace);
-			pos += to_find.length();
+			line.replace(i, to_find.length(), to_replace);
+			i += to_replace.length();
 		}
 		replace << line;
+		if (!file.eof())
+			replace << std::endl;
 	}
+	if (!file.eof())
+		std::cerr << "Error while reading" << std::endl;
 }
 
 int main(int argc, char **argv)
 {
 	if (argc != 4)
 	{
-		std::cout << "Wrong number of argumnets" << std::endl;
+		std::cerr << "Wrong number of argumnets" << std::endl;
 		return (1);
 	}
 
@@ -38,7 +42,7 @@ int main(int argc, char **argv)
 	replace.open(new_file + ".replace", std::ofstream::trunc);
 	if (file.fail() || replace.fail() || !to_find.length() || !to_replace.length())
 	{
-		std::cout << "Error with arguments (no such file, error while opening, empty argumnets)" << std::endl;
+		std::cerr << "Error with arguments (no such file, error while opening, empty arguments)" << std::endl;
 		return (1);
 	}
 	sed(file, replace, to_find, to_replace);
